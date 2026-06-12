7206d86f5e003ce343efa96631347e321bd21b6a
DeborahOlaboye <deboraholaboye@gmail.com>
2026-06-12 15:32:31 +0100

    Lexiq.sol: add ROUND_DURATION, MAX_WORDS, STAKE_THRESHOLD constants and RoundState enum

--- contracts/contracts/Lexiq.sol
+++ contracts/contracts/Lexiq.sol
@@ -8,6 +8,12 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 contract Lexiq is Ownable, ReentrancyGuard {
     IERC20 public immutable usdm;
 
+    uint32 public constant ROUND_DURATION  = 90;
+    uint8  public constant MAX_WORDS       = 15;
+    uint8  public constant STAKE_THRESHOLD = 10;
+
+    enum RoundState { ACTIVE, FINISHED }
+
     constructor(address _usdm) Ownable(msg.sender) {
         usdm = IERC20(_usdm);
     }
